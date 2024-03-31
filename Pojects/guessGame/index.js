3a7d20d80500a37de945323848c2f8d41bff2df2
unknown <samual.halder2020@gmail.com>
2024-04-01 00:41:44 +0530

    2nd commit

--- Pojects/guessGame/index.js
+++ Pojects/guessGame/index.js
@@ -1,15 +1,66 @@
-const genNum = Math.round(Math.random() * 100);
+let random = parseInt(Math.random() * 100 + 1);
+const inptField = document.querySelector("#guessField");
 const btn = document.querySelector("#subt");
-btn.addEventListener("click", (e) => {
-  e.preventDefault();
-  const number = document.querySelector("#guessField").value;
-  if (number === genNum) {
-    const message = "Wright ans";
-    const divv = document.querySelector(".resultParas").appendChild(message);
+const message = document.querySelector(".lowOrHi");
+const guessArrey = document.querySelector(".guesses");
+const guessRem = document.querySelector(".lastResult");
+const insertDiv = document.querySelector(".resultParas");
+const p = document.createElement("p");
+
+let guesses = [];
+let guessCnt = 3;
+guessRem.innerHTML = guessCnt;
+const play = true;
+if (play) {
+  btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    let num = parseInt(inptField.value);
+    console.log(num);
+    checkInp(num);
+  });
+}
+
+const checkInp = (num) => {
+  if (isNaN(num)) {
+    alerting("enter a number ");
+  } else if (num > 100) {
+    alerting("enter a number from 1-100");
+  } else if (isNaN(num)) {
+    alerting("enter a number from 1-100");
   } else {
-    const message = "Wrong ans";
-    const divv = document.querySelector(".resultParas").appendChild(message);
+    compare(num);
   }
-  console.log(genNum);
-  console.log("clivk");
-});
+};
+
+const compare = (num) => {
+  guessArrey.innerHTML += `${num},`;
+  if (guessCnt <= 0) {
+    endGame("Opps Game Ended try next time, the number was");
+  }
+  guessCnt--;
+  guessRem.innerHTML = `${guessCnt}`;
+  if (num < random) {
+    message.innerHTML = "number is  tooo low";
+  } else if (num > random) {
+    message.innerHTML = "number is too big";
+  } else {
+    endGame("bulls eye!!!");
+  }
+  inptField.value = "";
+  console.log(guessRem);
+  console.log(guesses);
+};
+const alerting = (msg) => {
+  alert(msg);
+  inptField.value = "";
+};
+
+const endGame = (msg) => {
+  guessCnt = 3;
+  guesses = [];
+  message.innerHTML = `${msg} ${random}`;
+
+  random = parseInt(Math.random() * 100 + 1);
+
+  console.log(random);
+};
